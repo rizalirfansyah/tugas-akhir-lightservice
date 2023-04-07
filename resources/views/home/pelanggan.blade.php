1955b9ca0834c647a89c1d2db7d7ed47cc2eb482
Rizal Irfansyah <rizalfriendchicken@student.ittelkom-sby.ac.id>
2023-04-07 08:40:48 +0700

    fix frontend table pelanggan

--- resources/views/home/pelanggan.blade.php
+++ resources/views/home/pelanggan.blade.php
@@ -87,9 +87,6 @@
                     <th class="px-4 py-3">Telepon</th>
                     <th class="px-4 py-3">Chat</th>
                     <th class="px-4 py-3">Alamat Pelanggan</th>
-                    <th class="px-4 py-3">Proses Servis</th>
-                    <th class="px-4 py-3">Bisa Diambil</th>
-                    <th class="px-4 py-3">Sudah diambil</th>
                     <th class="px-4 py-3">Total Servis</th>
                     <th class="px-4 py-3 text-center">Pilihan</th>
                     </tr>
@@ -107,9 +104,6 @@
                         </a>
                     </td>
                     <td class="px-4 py-3">Test alamat</td>
-                    <td class="px-4 py-3">{{ $customer->proses_servis }}</td>
-                    <td class="px-4 py-3">{{ $customer->bisa_diambil }}</td>
-                    <td class="px-4 py-3">{{ $customer->sudah_diambil }}</td>
                     <td class="px-4 py-3">{{ $customer->total_servis }}</td>
                     <td class="px-4 py-3">
                         <div class="w-30 items-center">
@@ -121,7 +115,7 @@
                             <div id="edit-cust-modal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] md:h-full">
                             <div class="relative w-full h-full max-w-md md:h-auto">
                                 <!-- Modal content -->
-                                <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                <div class="relative bg-blue-300 rounded-lg shadow dark:bg-gray-700">
                                     <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="edit-cust-modal">
                                         <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                                         <span class="sr-only">Close modal</span>
