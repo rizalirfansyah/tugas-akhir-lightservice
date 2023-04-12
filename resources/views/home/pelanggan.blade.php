15ad9b4bd6c2161af051ba7fb3e9096095f3498c
Rizal Irfansyah <rizalfriendchicken@student.ittelkom-sby.ac.id>
2023-04-12 21:09:25 +0700

    modal pelanggan

--- resources/views/home/pelanggan.blade.php
+++ resources/views/home/pelanggan.blade.php
@@ -107,7 +107,7 @@
                     <td class="px-4 py-4">
                         <div class="w-1 items-center 2xl:flex">
 
-                            {{-- modal ubah data --}}
+                            {{-- button modal ubah data --}}
                             <button data-modal-target="#edit-cust-modal{{ $customer->id }}" data-modal-toggle="edit-cust-modal{{ $customer->id }}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-2 text-center">
                             Ubah
                             </button>
@@ -147,12 +147,43 @@
                             </div>
                         </div>
 
+                        <div class="w-1 items-center 2xl:flex">
+
+                            {{-- buton modal hapus data --}}
+                            <button data-modal-target="#hapus-cust-modal{{ $customer->id }}" data-modal-toggle="hapus-cust-modal{{ $customer->id }}" class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-2 ml-2 text-center">
+                            Hapus
+                            </button>
+                            <!-- Main modal hapus data pelanggan -->
+                            <div id="hapus-cust-modal{{ $customer->id }}" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] md:h-full">
+                            <div class="relative w-full h-full max-w-md md:h-auto">
+                                <!-- Modal content -->
+                                <div class="relative bg-blue-300 rounded-lg shadow dark:bg-gray-700">
+                                    <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="hapus-cust-modal{{ $customer->id }}">
+                                        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                        <span class="sr-only">Close modal</span>
+                                    </button>
+                                    
+                                    <div class="px-6 py-6 lg:px-8">
+                                        <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Anda yakin ingin menghapus?</h3>
+                                        <div class="flex items-center space-x-2">
+                                            <form action="{{ route('pelanggan.destroy', $customer->id ) }}" method="POST">
+                                                @csrf
+                                                @method('delete')
+                                                <button class="mt-3 ml-1 px-2 py-2 text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Hapus</button>
+                                            </form>
+                                            <button class=" ml-1 px-2 py-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" data-modal-hide="hapus-cust-modal{{ $customer->id }}">Kembali</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
                         {{-- button hapus data --}}
-                        <form action="{{ route('pelanggan.destroy', $customer->id ) }}" method="POST">
+                        {{-- <form action="{{ route('pelanggan.destroy', $customer->id ) }}" method="POST">
                             @csrf
                             @method('delete')
                         <button class="mt-3 ml-1 px-2 py-2 text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Hapus</button>
-                        </form>
+                        </form> --}}
                     </td>
                     </tr>
 
